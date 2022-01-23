fa33d5fe45c3dcfddfae34656f09671d31b5f01d
Frederic Deschenes <frederic.deschenes1@gmail.com>
2022-01-23 07:48:21 -0500

    Move creation of car rows to factory

--- Guidomia/Features/Car List/CarListInteractor.swift
+++ Guidomia/Features/Car List/CarListInteractor.swift
@@ -25,43 +25,10 @@ class CarListInteractor {
         let result = await self.carsStore.getCars()
         switch result {
         case let .success(cars):
-            // TODO: Move the making of these rows to factories
-            var carRows = cars.flatMap({
-                return [
-                    CarListRow.car(item: .init(
-                        carImage: $0.image,
-                        carName: $0.model,
-                        // Formatting using a divided string could be improved by moving to an extension supporting
-                        // millions and billions as well
-                        carPrice: String(format: NSLocalizedString("CarList.Price", comment: ""), Int($0.customerPrice / 1000)),
-                        carRating: Double($0.rating),
-                        // The JSON data contains empty pros & cons items. Filter these out as well
-                        prosList: $0.prosList.filter({ !$0.isEmpty }),
-                        consList: $0.consList.filter({ !$0.isEmpty }),
-                        isExpanded: false
-                    )),
-                    CarListRow.separator
-                ]
-            })
-            // Remove the last separator cell as there is no car below it
-            carRows.removeLast()
-            // The first item in the list is expanded by default
-            switch carRows.first {
-            case var .car(item):
-                item.isExpanded = true
-                carRows[0] = .car(item: item)
-            default: break
-            }
-            
             viewController?.showData(sections: [
-                .init(rows: [
-                    CarListRow.header(item: .init(
-                        backgroundImage: UIImage(named: "Tacoma") ?? UIImage(),
-                        title: "Tacoma 2021",
-                        subtitle: NSLocalizedString("CarList.Header.CallToAction", comment: "")))
-                ]),
-                .init(rows: [.filter]),
-                .init(rows: carRows)
+                .init(rows: [CarListRowFactory.makeHeader()]),
+                .init(rows: [CarListRowFactory.makeFilter()]),
+                .init(rows: CarListRowFactory.makeCars(cars: cars))
             ])
         case let .failure(error):
             // TODO: Show error
@@ -72,12 +39,9 @@ class CarListInteractor {
 
 extension CarListInteractor: CarListInteractorProtocol {
     func onViewDidLoad() {
-        viewController?.showData(sections: [.init(rows: [
-            .header(item: .init(
-                backgroundImage: UIImage(named: "Tacoma") ?? UIImage(),
-                title: "Tacoma 2021",
-                subtitle: NSLocalizedString("CarList.Header.CallToAction", comment: "")))
-        ])])
+        viewController?.showData(sections: [
+            .init(rows: [CarListRowFactory.makeHeader()])
+        ])
         
         // Request the data asynchronously
         Task {
--- Guidomia/Features/Car List/CarListRow.swift
+++ Guidomia/Features/Car List/CarListRow.swift
@@ -5,11 +5,62 @@
 //  Created by Frederic Deschenes on 2022-01-21.
 //
 
-import Foundation
+import UIKit
 
 enum CarListRow {
     case header(item: CarListHeaderTableViewCell.Item)
     case filter
     case car(item: CarTableViewCell.Item)
     case separator
+}
+
+final class CarListRowFactory {
+    
+    static func makeHeader() -> CarListRow {
+        return .header(item: .init(
+            backgroundImage: UIImage(named: "Tacoma") ?? UIImage(),
+            title: "Tacoma 2021",
+            subtitle: NSLocalizedString("CarList.Header.CallToAction", comment: "")))
+    }
+    
+    static func makeFilter() -> CarListRow {
+        return .filter
+    }
+
+    static func makeCars(cars: [Car]) -> [CarListRow] {
+        // Aggregate all car rows and append them with a separator cell
+        var carRows = cars.flatMap({
+            [self.makeCar(car: $0), self.makeSeparator()]
+        })
+        // Remove the last separator cell as there is no car below it
+        carRows.removeLast()
+        // The first item in the list is expanded by default
+        switch carRows.first {
+        case var .car(item):
+            item.isExpanded = true
+            carRows[0] = .car(item: item)
+        default: break
+        }
+        
+        return carRows
+    }
+    
+    static func makeCar(car: Car) -> CarListRow {
+        return CarListRow.car(item: .init(
+            carImage: car.image,
+            carName: car.model,
+            // Formatting using a divided string could be improved by moving to an extension supporting
+            // millions and billions as well
+            carPrice: String(format: NSLocalizedString("CarList.Price", comment: ""), Int(car.customerPrice / 1000)),
+            carRating: Double(car.rating),
+            // The JSON data contains empty pros & cons items. Filter these out as well
+            prosList: car.prosList.filter({ !$0.isEmpty }),
+            consList: car.consList.filter({ !$0.isEmpty }),
+            isExpanded: false
+        ))
+    }
+    
+    static func makeSeparator() -> CarListRow {
+        return .separator
+    }
 }
